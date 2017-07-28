d8e9377c4db8852d8310c75eea5a84ae09a5c16f
BunnyEarsLawyer <smileumatter@gmail.com>
2017-07-27 23:39:27 -0400

    Separating notes and building multiplication table

--- Jumping_into_Cpp/ch14/sample_pointer_arithmetic.cpp
+++ Jumping_into_Cpp/ch14/sample_pointer_arithmetic.cpp
@@ -4,42 +4,54 @@
 //   $g++ -g name_of_file.cpp -o blah_binary
 // How to debug: 
 //   $gdb ./blah_binary
+
+/*
 //
-// Using GDB Dashboard
-// (gdb) break main
-// (gdb) n
-// (gdb) s
-// (gdb) info locals 
-// (gdb) list
-// (gdb) c
-// (gdb) E.g. >>> print p_p_table
-// (gdb) E.g. >>> print *p_p_table
-// (gdb) E.g. >>> print *(*p_p_table)            // 0
-// (gdb) E.g. >>> print *(*p_p_table + 1)        // 1
-// (gdb) E.g. >>> print *(*p_p_table + 1) + 1    // 2
-// (gdb) b 72 
-// (gdb) c // skips to break I set
+// p_p_table points to an array, its memory box contains an address
+// The beginning of an array is also a pointer, its memory box contains address to an int
+// Dereferencing twice will obtain the value of the int.
 //
-// How to watch memory 
-// (gdb) E.g. >>> dashboard memory watch 0x603010 4
-// (gdb) p *p_p_table@3  // @ treats parts of memory as arrays. 
-// $36 = {[0] = 0x603030, [1] = 0x603050, [2] = 0x0}
-// >>> print *(p_p_table)
-// $48 = (int *) 0x603030  
-// >>> print *(p_p_table + 1)
-// $49 = (int *) 0x603050
-//
-// How to set an expression 
-// (gdb) set $i = 0
-// (gdb) print $i 
-// (gdb) dashboard expression watch $i
+// Because the type is int, the sizeof(int) is 4 ... in my compiler
+
+// REMEMBER: memory addresses is 48 bits long.
+// Why do 64 bit systems have only 48 bit address space?
+//://stackoverflow.com/questions/6716946/why-do-64-bit-systems-have-only-a-48-bit-address-space
+
+// x86-64 (Also known as AMD64 and Intel 64) = 64 bit version of the x86 instruction set.
+
+// For example, a single byte can have values ranging from 00000000 to 11111111 in binary form, 
+// but this may be more conveniently represented as 00 to FF in hexadecimal.
+
+// $92 Quatdrillion...
+// The number 9,223,372,036,854,775,807, equivalent to the hexadecimal 
+// value 7FFF,FFFF,FFFF,FFFF16, is the maximum value for a 64-bit signed integer in computing.
+
+// Today an int is 4 bytes on 32 and 64 bit systems.
+// Memory, here, is separated by steps of 8? Adding 1 to a pointer 
+// will take you to the next memory box
+// 
+>>> p **p_p_table@2
+$7 = {[0] = 0, [1] = 0}
+>>> p **(p_p_table + 1)@2
+$8 = {[0] = 1, [1] = 1}
+>>> p p_p_table
+$9 = (int **) 0x603010
+>>> p p_p_table + 1
+$10 = (int **) 0x603018
+>>> p *(p_p_table + 1)
+$11 = (int *) 0x603050
+>>> p **(p_p_table + 1)
+$12 = 1
+
+*/
 
 
 #include <iostream>
 
 //
-// Builds a two dimensional multiplication table with arbitrary sizes for the two dimensions
+// This is for showing how to use pointer arithmetic
 //
+
 
 int main()
 {
@@ -47,7 +59,7 @@ int main()
     //
     // Pointer to array holding n pointers
     //
-    int n = 8;
+    int n = 3;
 
     int array_size = n - 1;
 
@@ -69,13 +81,17 @@ int main()
     // Now you can use the allocated memory like a 2D array
     //
 
+    int counter = 0;
+
     for (int ii = 0; ii < n; ++ii)
     {
 
         for (int jj = 0; jj < n; ++jj)
         {
 
-            p_p_table[ii][jj] = ii*jj;
+            p_p_table[ii][jj] = counter;
+
+            ++counter;
  
         }
 
@@ -83,9 +99,6 @@ int main()
 
     std::cout  << "sizeof(int)" << "\n "
                << sizeof(int) << std::endl;
-    std::cout  << "array_size = n - 1" << "\n "
-               << array_size << std::endl;
-
 
     //
     // Printing out content
@@ -97,12 +110,8 @@ int main()
         {
 
             std::cout  << "(" << ii << "," <<  jj << ") = "
-                       << p_p_table[ii][jj] << " ";
-   
-            if( jj == n - 1 )
-            {
-                std::cout << "." << std::endl;
-            }
+                       << p_p_table[ii][jj] << std::endl;
+
  
         }
 
@@ -133,4 +142,3 @@ int main()
     int x = 4; 
 
 }
-
