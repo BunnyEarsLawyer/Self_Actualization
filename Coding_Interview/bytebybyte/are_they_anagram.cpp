37089c63c8314c08b89d3f37843b0eb77f03de3e
BunnyEarsLawyer <smileumatter@gmail.com>
2017-09-18 22:20:21 -0400

    Comparing against one another

--- Coding_Interview/bytebybyte/are_they_anagram.cpp
+++ Coding_Interview/bytebybyte/are_they_anagram.cpp
@@ -41,25 +41,61 @@ int main()
 {
 
     //auto words = {"ax,", "xa", "m", "\n"};
-    vector<string> words = {"1 ", "2 ", "3 "};
+    vector<string> words = {"ax", "xa", "m"};
+//    vector<string> words = {"1 ", "2 ", "3 "};
 
-    for ( vector<string>::iterator ii = words.begin(); ii != words.end(); ++ii )
-    {
+    vector<string> result;
 
-        for ( vector<string>::iterator jj = words.begin(); jj != words.end(); ++jj )
-        {
-            cout << *ii;
-        }
-
-    }
-
+    //
     // O-notation
     // source: https://stackoverflow.com/questions/2307283/what-does-olog-n-mean-exactly
     //
+    // remove/erase idiom: https://stackoverflow.com/questions/347441/erasing-elements-from-a-vector
+
+    // I'll just save the non-anagrams
+    for ( vector<string>::iterator ii = words.begin(); ii != words.end(); ++ii )
+    {
+        for ( vector<string>::iterator jj = words.begin(); jj != words.end(); ++jj )
+        {
+            // Don't compare against yourself only to others
+            // (This is not good life advice!)
+            if (&*ii != &*jj) 
+            {
+//                cout << "ii " << *ii << endl;
+//                cout << "jj " << *jj << endl;
+                  cout << "where " << *ii << " " << *jj << endl; 
+                  if( is_anagram(*ii, *jj) )
+                  {
+                     cout << "remove " << *ii << " " << *jj << endl; 
+                     // Remove() does not really remove things.
+                     // b/c 'remove()' DOES return the container's new 'end()'
+ //                    words.erase(remove(words.begin(), words.end(), *ii));
+//                     words.erase(
+//                     remove(words.begin(), words.end(), *jj);
+                     ii = words.erase(ii);
+                  } 
+            }
+            else
+            {
+                cout << "same" << *ii << " " << *jj << endl; 
+            }
+        }
+    }
+
+    // Boost solution:
+    // boost::remove_if(words, bind(&Player::is_anagram, _1)<=0 );
+
+//    auto it = std::find(myVec.begin(), myVec.end(), obj37);
+//    if (it != myVec.end()) { myVec.erase(it); }
+
+
+    // Prints final result
+    cout << "Result\n";
     for (const string& word : words)
     {
         cout << word;
     }
+
     return 0;
 
 }
