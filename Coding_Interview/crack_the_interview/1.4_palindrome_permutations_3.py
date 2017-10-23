7a97eb0479eb264a0bed180f0630e910270b3ddb
BunnyEarsLawyer <smileumatter@gmail.com>
2017-10-23 19:49:42 -0400

    One permutation 1.4

--- Coding_Interview/crack_the_interview/1.4_palindrome_permutations_3.py
+++ Coding_Interview/crack_the_interview/1.4_palindrome_permutations_3.py
@@ -10,28 +10,42 @@ def can_be_palindrome(ss):
    # convert all to lowercase
    a_list = []
    b_list = []
+   c_list = []
+   d_list = []
    for idx in range(len(my_list)):
        curr_char = my_list[idx]
        curr_count = my_list.count(my_list[idx])
        if curr_char not in b_list:
-           a_list.append(curr_count) 
-           b_list.append(curr_char)
-   print(a_list) 
-   print(b_list) 
+           if (curr_count % 2 == 0):
+               a_list.append(curr_count) 
+               b_list.append(curr_char)
+           else:
+               c_list.append(curr_count)
+               d_list.append(curr_char)
    ab = zip(a_list, b_list)
+   cd = zip(c_list, d_list)
    ab.sort() 
    print(ab) 
+   print(cd) 
+
 
    half_pe = []
    half_ep = []
-   if(ab.count(1) > 1):
-       return False
+   if(cd.count(1) > 1):
+       print('CANNOT MAKE PALINDROME')
    else:
+       less_than_two = list(filter(lambda x: x[0] < 2, cd))
+#      out_tup = [i for i in ab if i[1] > 2]
+       less_than_two = np.array(less_than_two)
+       # Gets all rows along the first dim.
+       single_char = less_than_two[:,1]
+       print(less_than_two) 
        ab = np.array(ab) 
        half_pe = ab[:,1]
        half_ep = list(reversed(half_pe)) 
        half_pe = list(half_pe)
-       palindrome = half_pe + half_ep
+       single_char = list(single_char)
+       palindrome = half_pe + single_char +  half_ep
        print(palindrome)
 
 sss = 'tacocat'
