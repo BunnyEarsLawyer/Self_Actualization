386ab5e17cf40d81c1e647dc12e2e97521a5da49
BunnyEarsLawyer <smileumatter@gmail.com>
2017-10-24 12:33:23 -0400

    Solved P1.7 with 23 mins to spare

---
+++ Coding_Interview/crack_the_interview/1.7_rotate_matrix.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int rr = 3;
+    int cc = 3;
+    int matrix_nn [rr][cc];
+    int rot_nn [rr][cc];
+    int counter = 0;
+    for (int ii = 0; ii < rr; ++ii) {
+        for (int jj = 0; jj < cc; ++jj) {
+            matrix_nn[ii][jj] = counter;
+            counter++;
+            cout << matrix_nn[ii][jj] << endl;
+        }
+    } 
+    int rot_ii = 0;
+    for (int ii = 0; ii < rr; ++ii) {
+        for (int jj = 0; jj < cc; ++jj) {
+            rot_ii = rr - ii - 1;
+            rot_nn[ii][jj] = matrix_nn[jj][rot_ii];
+            cout << rot_nn[ii][jj] << endl;
+        }
+    } 
+    cout << "hi" << endl;
+ 
+}
