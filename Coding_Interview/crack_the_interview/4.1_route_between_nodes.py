5cad49ea56760bdebb35b3075d9fddcec7f9eebe
BunnyEarsLawyer <smileumatter@gmail.com>
2018-01-14 13:57:44 -0500

    3.1 written solution, 4.1 started

---
+++ Coding_Interview/crack_the_interview/4.1_route_between_nodes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+Given a directed graph, design an algorithm to find out whether there is a route b/t 2 notes
+
+Directed graph
+
+a -> b -> c
+     ^    ^
+     |    |
+     e <- d -> z
+
+'Directed Edges'  = One way road/arrow
+'Undirected Edges' = 2 way streets, don't even bother with double arrows.
+
+'Edges' = The connectors (think of them as roads)
+
+- Unlike a tree, you don't necessarily reach all the nodes from a single node. 
+
+"""
+
+
+
